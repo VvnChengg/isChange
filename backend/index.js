07924f0a49454ab6a32a87420109957593e02dc8
“朱盈穎” <r11725011@ntu.edu.tw>
2024-04-03 22:48:21 +0800

    test: mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors') ;
+
+require('dotenv').config(); // 加了這行就可以抓到 port
 
 const app = express();
 
@@ -8,8 +13,18 @@ app.use(cors());
 
 // app.use('/api/名稱', 路徑)
 
-const server = app.listen(process.env.PORT, '0.0.0.0', () =>
-    console.log(`Server running on port http://localhost:${process.env.PORT}`),
+// const server = app.listen(process.env.PORT, '0.0.0.0', () =>
+//     console.log(`Server running on port http://localhost:${process.env.PORT}`),
+// );
+
+
+// test if connected to mongo DB: localhost:3000/api/test
+// webpage should show 4 names in DB
+const databaseRoutes = require('./src/routes/test');
+app.use('/api', databaseRoutes);
+
+const server = app.listen(process.env.PORT || 3000, () =>
+    console.log(`Server running on port http://localhost:${server.address().port}`),
 );
 
 // Connect to MongoDB
