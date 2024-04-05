4883771eac008da04ee9087fdda1258968c64a10
Samantha Chu <samasa112233@gmail.com>
2024-04-05 23:05:06 +0800

    feat: add DB schema; add dbname in index.js; successfully get member info

--- backend/src/routes/test.js
+++ backend/src/routes/test.js
@@ -2,17 +2,16 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-// 定義路由
-router.get('/test', async (req, res) => {
+const Member = require('../models/member');
+
+// get member data
+router.get('/members', async (req, res) => {
     try {
-        // 獲取所有資料庫的名稱
-        const databases = await mongoose.connection.db.admin().listDatabases();
-        const databaseNames = databases.databases.map(db => db.name);
-        console.log('List of databases:', databaseNames);
-        res.json({ databases: databaseNames });
+        const members = await Member.find({}, 'username intro exchage_school_name region'); // column names
+        res.json({ members });
     } catch (error) {
-        console.error('Failed to list databases:', error);
-        res.status(500).json({ error: 'Failed to list databases' });
+        console.error('Failed to fetch members:', error);
+        res.status(500).json({ error: 'Failed to fetch members' });
     }
 });
 
