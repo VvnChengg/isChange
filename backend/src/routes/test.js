532d73a11f48f3776b06f561b763896d5354c3be
r12725063 <r12725063@ntu.edu.tw>
2024-04-06 03:50:43 +0800

    feat: add generateArticle.js in src/scripts; add GET/article in route/test.js

--- backend/src/routes/test.js
+++ backend/src/routes/test.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Member = require('../models/member');
+const Article = require('../models/article');
+
 
 // get member data
 router.get('/members', async (req, res) => {
@@ -12,6 +14,18 @@ router.get('/members', async (req, res) => {
     } catch (error) {
         console.error('Failed to fetch members:', error);
         res.status(500).json({ error: 'Failed to fetch members' });
+    }
+});
+
+// get article data
+router.get('/article', async (req, res) => {
+    try {
+        // const article = await Article.find({}, 'article_title article_pic post_date status creator_id'); // column names
+        const article = await Article.find({}, 'creator_id like_by_user_ids save_by_user_ids comment_ids'); // column names
+        res.json({ article });
+    } catch (error) {
+        console.error('Failed to fetch article:', error);
+        res.status(500).json({ error: 'Failed to fetch article' });
     }
 });
 
