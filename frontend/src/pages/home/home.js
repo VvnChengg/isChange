a55110f344b99c32dabe525e4e19e09fcf3c8afa
r12725023 <jiacian.yang@gmail.com>
2024-04-08 21:59:10 +0800

    style: home page post

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,5 +1,15 @@
+import { PostContainer } from "./home-style";
+
+import Post from "../../components/Post";
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <PostContainer>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+        </PostContainer>
     )
 }
