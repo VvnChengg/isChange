9910d56892d93a1f2bab3291069cf6d05c2e472c
Claire <r12725029@ntu.edu.tw>
2024-04-10 23:26:21 +0800

    feat: add member edit page

--- backend/src/controllers/member.js
+++ backend/src/controllers/member.js
@@ -1,19 +1,26 @@
 const Member = require("../models/member");
+const MemberAuth = require("../models/member-auth");
+const bcrypt = require("bcrypt");
 
 //查看自己的個人資料（Member)
 const showMember = async (req, res) => {
-  const username = req.body.username;
-  res.json({ username });
+  const { userId, username } = req.body;
 
-  if (!username) {
-    return res.status(400).json({ error: "未取得使用者名稱" });
+  if (!userId) {
+    return res.status(400).json({ error: "未取得使用者資訊" });
   }
   try {
-    const user = await Member.findOne({ username: username }, "intro photo");
+    const userAuth = await MemberAuth.findOne({ _id: userId });
+    const user = await Member.findOne({
+      _id: userAuth.user_id,
+      username: username,
+    });
+
     if (!user) {
+      console.log(`Member not found with ID: ${userId}`);
       return res.status(404).json({ error: "會員不存在" });
     }
-    res.json({ user });
+    return res.json(user);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
@@ -22,61 +29,127 @@ const showMember = async (req, res) => {
 
 //修改個人資料（Member)
 const modifyMember = async (req, res) => {
-  const { username, intro, photo } = req.body;
-  try {
-    const updatedUser = await Member.findOneAndUpdate(
-      { username: username },
-      {
-        $set: {
-          intro: intro,
-          photo: photo,
+  const {
+    userId,
+    username,
+    intro,
+    photo,
+    exchange_school_email,
+    origin_password,
+    new_password,
+  } = req.body;
+
+  if (!origin_password || !new_password) {
+    try {
+      const userAuth = await MemberAuth.findOne({ _id: userId });
+      const updatedUser = await Member.findOneAndUpdate(
+        { _id: userAuth.user_id, username: username },
+        {
+          $set: {
+            intro: intro,
+            photo: photo,
+            exchange_school_email: exchange_school_email,
+          },
         },
-      },
-      { new: true }
-    );
+        { new: true, runValidators: true }
+      );
 
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
+      if (!updatedUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      return res.json({
+        status: "success",
+        message: "User information updated successfully",
+        data: updatedUser,
+      });
+    } catch (error) {
+      if (error.name === "ValidationError") {
+        const errors = Object.values(error.errors).map((err) => err.message);
+        return res.status(400).json({ error: errors.join(", ") });
+      }
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
     }
+  } else {
+    try {
+      const user = await MemberAuth.findOne({ _id: userId });
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      const passwordMatch = await bcrypt.compare(
+        origin_password,
+        user.password
+      );
 
-    return res.json({
-      status: "success",
-      message: "User information updated successfully",
-      data: updatedUser,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error" });
+      if (passwordMatch) {
+        const h_Password = await hashPassword(new_password);
+
+        const updatedUser = await MemberAuth.findOneAndUpdate(
+          { _id: userId },
+          {
+            $set: {
+              password: h_Password,
+            },
+          },
+          { new: true, runValidators: true }
+        );
+        return res.json({
+          status: "success",
+          message: "密碼修改成功",
+        });
+      }
+      return res.json({
+        status: "error",
+        message: "密碼錯誤",
+      });
+    } catch (error) {
+      if (error.name === "ValidationError") {
+        const errors = Object.values(error.errors).map((err) => err.message);
+        return res.status(400).json({ error: errors.join(", ") });
+      }
+      console.error(error);
+      return res.status(500).json({ error: "修改失敗請稍後再試" });
+    }
   }
 };
 
 //查看別人的個人資料（Member）
 const showMemberDetail = async (req, res) => {
-  const { viewer_username, observed_username } = req.body;
-  if (!viewer_username || !observed_username) {
+  const userId = req.body;
+  const observed_username = req.params.uid;
+
+  if (!userId || !observed_username) {
     return res.status(400).json({ error: "Username is missing" });
   }
   try {
-    const user = await Member.findOne(
+    const observed_user = await Member.findOne(
       { username: observed_username },
       "intro photo exchange_school_name region"
     );
-    if (!user) {
+    if (!observed_user) {
       return res.status(404).json({ error: "User not found" });
     }
-    res.json({ user });
+    res.json(observed_user);
   } catch (error) {
     console.error("Failed to fetch user info:", error);
     res.status(500).json({ error: "Failed to fetch user info" });
   }
 };
 
-//修改密碼（MemberAuth）
-const modifyPassword = async (req, res) => {};
+//加密
+const hashPassword = async (password) => {
+  try {
+    const saltRounds = 10; // This determines the complexity of the hash
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error("Failed to hash password");
+  }
+};
 
 module.exports = {
   showMember,
   modifyMember,
   showMemberDetail,
-  modifyPassword,
 };
