c27529aa7ea9d2128f3a4420abb73498b8ff315a
lala72841 <96458868+lala72841@users.noreply.github.com>
2024-04-10 17:32:19 +0800

    feat: add login and register functions (#7)
    * feat: added member-auth api(LOR,login, verifiedMember) ; added member api (showInfo, modifyInfo, showDetailedInfo)
    
    * feat: generate jwt tokens when login and token verification middleware
    
    * style: rename cookieJwtAuth middleware
    
    * feat: add verification code mail sender and verifying function
    
    * feat: create memberauth documents, add deleteMember function for internal testing
    
    * fix: if is_verified, shouldn't verify verification code; if password is not null, shouldn't set password for the first time
    
    * feat: Member collection added user_id
    
    * feat: created member when creating memberauth
    
    * feat: add member info edit controller
    
    * fix: memberAuth route fix

--- backend/src/models/member.js
+++ backend/src/models/member.js
@@ -1,51 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const memberSchema = new Schema({
-    username: {
-        type: String,
-        maxlength: 15,
-        required: true
+  username: {
+    type: String,
+    maxlength: 15,
+    required: true,
+  },
+  intro: {
+    type: String,
+    maxlength: 200,
+    default: null,
+  },
+  photo: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        // 檢查是否為合法的 URL 格式
+        return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(v);
+      },
+      message: "Invalid URL format",
     },
-    intro: {
-        type: String,
-        maxlength: 200
-    },
-    photo: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                // 檢查是否為合法的 URL 格式
-                return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(v);
-            },
-            message: 'Invalid URL format'
-        }
-    },
-    exchange_school_name: {
-        type: String,
-        maxlength: 50
-    },
-    region: {
-        type: String,
-        required: true
-    },
-    region_list: {
-        type: [String]
-    },
-    review_list: {
-        type: [Number]
-    },
-    recognition: {
-        type: Boolean
-    },
-    chat_ids: {
-        type: [Schema.Types.ObjectId], // 參考到 chat 集合的 ObjectId
-        ref: 'Chat' // 設置參考的集合名稱
-    },
-    follow_ids: {
-        type: [Schema.Types.ObjectId], // 參考到 chat 集合的 ObjectId
-        ref: 'Member' // 設置參考的集合名稱
-    }
+  },
+  exchange_school_name: {
+    type: String,
+    maxlength: 50,
+  },
+  exchange_school_email: {
+    type: String,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/, // 驗證 email 格式
+  },
+  region: {
+    type: String,
+    // required: true,
+  },
+  region_list: {
+    type: [String],
+  },
+  review_list: {
+    type: [Number],
+  },
+  recognition: {
+    type: Boolean,
+  },
+  chat_ids: {
+    type: [Schema.Types.ObjectId], // 參考到 chat 集合的 ObjectId
+    ref: "Chat", // 設置參考的集合名稱
+  },
+  follow_ids: {
+    type: [Schema.Types.ObjectId], // 參考到 chat 集合的 ObjectId
+    ref: "Member", // 設置參考的集合名稱
+  },
 });
 
-module.exports = mongoose.model('Member', memberSchema);
+module.exports = mongoose.model("Member", memberSchema);
