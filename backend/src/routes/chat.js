81a6609b211285b16e3dedb1240c83a2248d4657
Samantha Chu <samasa112233@gmail.com>
2024-04-12 17:08:38 +0800

    perf: revise chat/chatlist and chat/detail api

--- backend/src/routes/chat.js
+++ backend/src/routes/chat.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const chatController = require("../controllers/chat");
+const chatController = require("../controllers/chat.js");
+const validateToken = require("../../src/middlewares/validateToken");
 
-router.get("/check", chatController.checkChat);
-router.post("/create", chatController.createChat);
-router.get("/chatlist", chatController.getChatList);
-router.get("/detail/:cid", chatController.getChatDetail);
-router.post("/sendtext/:cid", chatController.sendMessage);
+router.get("/check", validateToken, chatController.checkChat);
+router.post("/create", validateToken, chatController.createChat);
+router.get("/chatlist", validateToken, chatController.getChatList);
+router.get("/detail/:cid", validateToken, chatController.getChatDetail);
+router.post("/sendtext/:cid", validateToken, chatController.sendMessage);
 
 module.exports = router;
