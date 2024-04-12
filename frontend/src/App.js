61dfb72da5d341b5575b026586f6f90929ae0082
r12725023 <jiacian.yang@gmail.com>
2024-04-12 22:29:21 +0800

    feat: create tour

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './styles/global.css';
 
 import Layout from './pages/layout';
 import Home from './pages/home';
+import TourCreate from './pages/tourCreate';
 
 import Header from './components/Header';
 
@@ -16,6 +17,9 @@ function App() {
           <Routes>
             <Route path='' element={<Layout />}>
               <Route path='/' element={<Home />} />
+              <Route path='/tour'>
+                <Route path='create' element={<TourCreate />} />
+              </Route>
             </Route>
           </Routes>
         </div>
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -10,5 +10,18 @@ export const api = {
             .catch(err => console.log(err))
         )
     },
+    createTour: (tour) => {
+        const token = window.localStorage.getItem('access_token');
+
+        return (
+            axios.post(hostname + '/tour/create', { tour }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(res => res.data)
+            .catch(err => console.log(err))
+        )
+    }
 };
   
