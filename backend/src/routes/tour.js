0767139a2e4c085cb4f809706d83d4f75e5c1014
Vivian Cheng <90235050+VvnChengg@users.noreply.github.com>
2024-04-13 21:43:58 +0800

    feat: add login authentication for tour (#16)

--- backend/src/routes/tour.js
+++ backend/src/routes/tour.js
@@ -1,14 +1,15 @@
 const express =  require('express');
 const tourApi = require('../controllers/tour.js');
+const validateToken = require("../../src/middlewares/validateToken");
 
 const router = express.Router();
 
-router.post('/create', tourApi.createTour);
+router.post('/create', validateToken, tourApi.createTour);
 router.get('/detail/:eid', tourApi.checkTourDetail);
-router.get('/edit/:eid', tourApi.checkEditingTour);
-router.put('/edit/:eid', tourApi.editTour);
-router.delete('/delete', tourApi.deleteTour);
-router.get('/list/established', tourApi.getEstablishedTours);
-router.get('/list/finished', tourApi.getFinishedTours);
+router.get('/edit/:eid', validateToken, tourApi.checkEditingTour);
+router.put('/edit/:eid', validateToken, tourApi.editTour);
+router.delete('/delete', validateToken, tourApi.deleteTour);
+router.get('/list/established', validateToken, tourApi.getEstablishedTours);
+router.get('/list/finished', validateToken, tourApi.getFinishedTours);
 
 module.exports = router;
