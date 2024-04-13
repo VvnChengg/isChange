4308d443c0882d18b4a01513e8d1d0def50c2393
Claire <r12725029@ntu.edu.tw>
2024-04-13 21:42:31 +0800

    fix: change HTTP method for login, from GET to POST

--- backend/src/routes/memberAuth.js
+++ backend/src/routes/memberAuth.js
@@ -4,7 +4,7 @@ const memberAuthController = require("../controllers/memberAuth");
 
 //會員註冊或登入
 router.get("/login-or-register", memberAuthController.LOR);
-router.get("/login", memberAuthController.login);
+router.post("/login", memberAuthController.login);
 router.post("/register", memberAuthController.registerMember);
 router.get("/register", memberAuthController.verifyRegisterMember);
 router.patch("/register", memberAuthController.verifiedMember);
