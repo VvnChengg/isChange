5002bfa05c1e6b8fa0302ffb50059eefb3b4db02
黃文新 <r11725018@ntu.edu.tw>
2024-04-14 18:00:45 +0800

    page化
    目前不知道為什麼用  const hostname = process.env.REACT_APP_API_HOSTNAME;
    都會變成 undefined
    
    StartPrivate 元件串接時需要抓一下那頁的userid跟username
    token 還沒研究出來怎麼用，但關掉token認證應該是沒問題了

--- frontend/src/components/PrivateMessage/ChatRoom.js
+++ frontend/src/components/PrivateMessage/ChatRoom.js
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ChatRoom.css';
+import  './ChatRoom.css';
 
 
 
-export default function ChatRoom({chatId}) {
+export default function ChatRoom({chatid}) {
   const hostname = 'http://localhost:3000/api';
+  //const hostname = process.env.REACT_APP_API_HOSTNAME;
   const [chatData, setChatData] = useState(null);
-  //const chatId = '6614e7491dd21a48510f2108'
-  const user_id = "660bbad71dd21a48510f209c";
-  const userId = "660bbad71dd21a48510f209c";
-  // const ChatId = window.localStorage.getItem('chat_id');
-  // const your_user_id = window.localStorage.getItem('user_id');
+  // const userId = "660bbad71dd21a48510f209c";
+  // const userId = window.localStorage.getItem('user_id');
   
   useEffect(() => {
     // 發送 API 請求
-    axios.get(`${hostname}/chat/detail/${chatId}`)
+    axios.get(`${hostname}/chat/detail/${chatid}`)
       .then(response => {
         // 設置獲取到的數據
         console.log(response.data);
@@ -25,25 +23,25 @@ export default function ChatRoom({chatId}) {
       .catch(error => {
         console.error('API 請求失敗:', error);
       });
-  }, [chatId]);
+  }, [chatid]);
 
   return (
     <div >
       {/* 確保 chatData 已經被設置，然後將其傳遞給 ChatRoom 組件 */}
       {chatData && (
-        <div className="chat-room-container" >
+        <div className="private-message-chat-room-container" >
           {/* 渲染聊天對象的頭像和用戶名 */}
-          <div className="chat-header">
+          <div className="private-message-chat-header">
             <img src={chatData.chat_to_photo} alt="chat-to-avatar" />
             <p>{chatData.chat_to_username}</p>
           </div>
 
           {/* 渲染訊息 */}
           {chatData.messages.map(message => (
-            <div className='message-container'>
-              <div className={`message-box ${message.sender_id === user_id ? "own-message" : ""}`} key={message._id}>
-                <p className="message-content">{message.content}</p>
-                <p className="timestamp">{new Date(message.timestamp).toLocaleString()}</p>
+            <div className='private-message-message-container'>
+              <div className={`private-message-message-box ${message.sender_id === userId ? "private-message-own-message" : ""}`} key={message._id}>
+                <p className="private-message-message-content">{message.content}</p>
+                <p className="private-message-timestamp">{new Date(message.timestamp).toLocaleString()}</p>
               </div>
             </div>
           ))}
