b6d2d69433f722b111721379467b79885bc5d4ce
r12725023 <jiacian.yang@gmail.com>
2024-04-18 00:26:19 +0800

    style: remove unused part

--- frontend/src/components/Tag/Tag.js
+++ frontend/src/components/Tag/Tag.js
@@ -3,14 +3,14 @@ import { FormattedMessage } from 'react-intl';
 
 export default function Tag({ type }) {
     const textDict = {
-        trans: { color: '0071D9', text: <FormattedMessage id='tag.trans' /> },
-        tour: { color: '0086BF', text: <FormattedMessage id='tag.tour' /> },
-        post: { color: '4DC0FF', text: <FormattedMessage id='tag.post' /> }
+        trans: <FormattedMessage id='tag.trans' />,
+        tour: <FormattedMessage id='tag.tour' />,
+        post: <FormattedMessage id='tag.post' />
     };
 
     return (
         <TagWrapper type={type}>
-            {textDict[type].text}
+            {textDict[type]}
         </TagWrapper>
     )
 }
