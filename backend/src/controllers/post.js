4804854a356907127224a25ae1cfd1681f9a37f0
TehTing <67617687+TehTing@users.noreply.github.com>
2024-04-21 23:50:31 +0800

    feat: add GET/post/detail/:pid (#49)
    1. 新增檢視文章內容 api : GET /post/detail/:pid
         a. 不需要 Token即可檢視
         b. 目前設定只能檢視已發佈的文章
    2. GET /post/all 的 res 新增"_id"，讓前端可以使用 _id 查詢並進入文章/揪團/商品頁面
    3. 修正 POST /post/create 讀取 req 的格式為 req.body

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -17,6 +17,7 @@ const getAllPosts = async (req, res, next) => {
         // 抽取文章需要的資訊並統一格式
         articles.forEach(article => {
             const item = {
+                _id: article._id,
                 title: article.article_title,
                 content: article.content,
                 type: "post",
@@ -31,6 +32,7 @@ const getAllPosts = async (req, res, next) => {
         // 抽取活動需要的資訊並統一格式
         events.forEach(event => {
             const item = {
+                _id: event._id,
                 title: event.event_title,
                 content: event.event_intro,
                 type: "tour",
@@ -50,6 +52,7 @@ const getAllPosts = async (req, res, next) => {
         // 抽取商品需要的資訊並統一格式
         products.forEach(product => {
             const item = {
+                _id:product._id,
                 title: product.product_title,
                 content: product.description,
                 type: "trans",
@@ -80,21 +83,38 @@ const getAllPosts = async (req, res, next) => {
     return res.status(200).json({ result });
 };
 
+const getPostDetail = async (req, res, next) => {
+    const { pid } = req.params;
+    // const {userId} = req.body;
+    let article;
+    try{
+        article = await Article.findOne({_id:pid, status:"complete"},);
+        if(!article){
+            return res.status(404).json({message:"找不到此文章"});
+        }
+        // console.log(article);
+        // console.log(article.creator_id, userId, JSON.stringify(article.creator_id)===JSON.stringify(userId));
+        // console.log(article.status==="draft");
+        // if(JSON.stringify(article.creator_id)!=JSON.stringify(userId) && article.status==="draft" ){
+        //     return res.status(401).json({message:"文章尚未發布"});
+        // }
+    }catch(error){
+        return res.status(400).json({message:error});
+    }
+    return res.status(200).json({article});
+};
+
 const getUserPosts = async (req, res, next) => {
     let articles;
     let result = [];
     const searchId = req.params.uid;
     const uId = req.body.userId;
     try {
-        // const user = await memberAuth.findOne({user_id: uId});
-        // if(!user){
-        //     return res.status(404).json({message: "User not found"});
-        // }
         // 若查看的不是自己的文章，只能看到已發佈的文章(狀態:complete)
-        if(uId == searchId){
-            articles = await Article.find({creator_id: searchId},);
-        }else{
-            articles = await Article.find({creator_id: searchId, status:"complete"},);
+        if (uId == searchId) {
+            articles = await Article.find({ creator_id: searchId },);
+        } else {
+            articles = await Article.find({ creator_id: searchId, status: "complete" },);
         }
         // 抽取文章需要的資訊並統一格式
         articles.forEach(article => {
@@ -124,17 +144,17 @@ const getUserPosts = async (req, res, next) => {
 };
 
 const createPost = async (req, res, next) => {
-    const postData = req.body.post;
+    const post = req.body;
     const uId = req.body.userId;
-    if (!postData) {
+    if (!post) {
         return res.status(404).json({ message: "未傳入文章創建資訊" });
     }
     try {
         newPost = new Article({
-            article_title: postData.title,
-            content: postData.content,
-            article_pic: postData.photo,
-            status: postData.status,
+            article_title: post.title,
+            content: post.content,
+            article_pic: post.photo,
+            status: post.status,
             creator_id: uId
         })
         await newPost.save();
@@ -149,19 +169,18 @@ const createPost = async (req, res, next) => {
 const updatePost = async (req, res, next) => {
     // 測試可用 http://localhost:3000/api/post/6617996b1067c62b7d70464e
     const uId = req.body.userId;
+    const pid = req.params.pid;
+    const updates = { article_title: req.body.title, content: req.body.content, status: req.body.status };
+    let post = await Article.findById(pid);
     try {
-        const pId = req.params.pid;
-        const updates = { article_title: req.body.title, content: req.body.content, status: req.body.status};
-        let post = await Article.findById(pId);
-
         // 檢查貼文是否存在
         if (!post) {
             return res.status(404).json({ message: '貼文不存在' });
         }
 
         // 檢查使用者是否有權限編輯文章
-        if(!post.creator_id.equals(uId)){
-            return res.status(401).json({message:"您沒有權限編輯此文章"});
+        if (!post.creator_id.equals(uId)) {
+            return res.status(401).json({ message: "您沒有權限編輯此文章" });
         }
 
         // 檢查更新內容是否符合規範
@@ -169,7 +188,7 @@ const updatePost = async (req, res, next) => {
         if (error) {
             return res.status(400).json({ error });
         }
-        post = await Article.findByIdAndUpdate(pId, updates, { new: true });
+        post = await Article.findByIdAndUpdate(pid, updates, { new: true });
         res.status(200).json({ message: '成功更新貼文' });
     } catch (err) {
         return res.status(500).json({ message: err.message });
@@ -177,30 +196,35 @@ const updatePost = async (req, res, next) => {
 }
 
 const deletePost = async (req, res, next) => {
-    const pId = req.params.pid;
+    const { pid } = req.params;
     const uId = req.body.userId;
     let post;
     try {
         // 找到貼文內容
-        post = await Article.findByIdAndDelete(pId);
-
+        console.log("pid: ", pid);
+        post = await Article.findOne({ "_id": pid },);
+        console.log("post", post);
         // 檢查是否有找到文章
         if (!post) {
             return res.status(404).json({ message: '貼文不存在' });
         }
 
         // 檢查使用者是否有權限刪除文章
-        if(!post.creator_id.equals(uId)){
-            return res.status(401).json({message:"您沒有權限刪除此文章"});
+        if (!post.creator_id.equals(uId)) {
+            return res.status(401).json({ message: "您沒有權限刪除此文章" });
         }
-        res.status(200).json({ message: '成功刪除貼文'});
+        Article.findOneAndDelete({ "_id": pid },);
+        res.status(200).json({ message: '成功刪除貼文' });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
+
+
 
 exports.getAllPosts = getAllPosts;
 exports.getUserPosts = getUserPosts;
 exports.createPost = createPost;
 exports.updatePost = updatePost;
 exports.deletePost = deletePost;
+exports.getPostDetail = getPostDetail;
