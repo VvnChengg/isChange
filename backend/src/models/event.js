57eabbd43cedceebcb3b511f065d3fc49b995015
Claire <r12725029@ntu.edu.tw>
2024-04-22 00:46:29 +0800

    modify regions for article, product, event as comments

--- backend/src/models/event.js
+++ backend/src/models/event.js
@@ -1,79 +1,84 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    event_title: {
-        type: String,
-        maxlength: 15,
-        required: true
+  event_title: {
+    type: String,
+    maxlength: 15,
+    required: true,
+  },
+  event_pic: {
+    data: Buffer,
+    contentType: String,
+  },
+  destination: {
+    type: String,
+    maxlength: 20,
+    required: true,
+    //type: { type: String },
+    //coordinates: [Number],
+  },
+  event_intro: {
+    type: String,
+    maxlength: 200,
+  },
+  start_time: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value < this.end_time;
+      },
+      message: "Start time must be before end time",
     },
-    event_pic:{
-        data: Buffer,
-        contentType: String,
+  },
+  end_time: {
+    type: Date,
+    required: true,
+  },
+  people_lb: {
+    type: Number,
+    validate: {
+      validator: function (value) {
+        return value < this.people_ub;
+      },
+      message: "People lower bound must be less than upper bound",
     },
-    destination: {
-        type: String,
-        maxlength: 20,
-        required: true
-    },
-    event_intro: {
-        type: String,
-        maxlength: 200
-    },
-    start_time: {
-        type: Date,
-        required: true,
-        validate: {
-            validator: function(value) {
-                return value < this.end_time;
-            },
-            message: 'Start time must be before end time'
-        }
-    },
-    end_time: {
-        type: Date,
-        required: true
-    },
-    people_lb: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-                return value < this.people_ub;
-            },
-            message: 'People lower bound must be less than upper bound'
-        }
-    },
-    people_ub: {
-        type: Number
-    },
-    creator_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Member',
-        required: true
-    },
-    status: {
-        type: String,
-        maxlength: 10,
-        required: true,
-        default: 'ongoing',
-        enum: ['ongoing', 'complete', 'end']
-    },
-    like_by_user_ids: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Member'
-    },
-    save_by_user_ids: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Member'
-    },
-    currency: {
-        type: String,
-        maxlength: 10,
-        enum: ['USD', 'GBP', 'EUR', 'TWD', 'CAD', 'AUD']
-    },
-    budget: {
-        type: Number
-    }
+  },
+  people_ub: {
+    type: Number,
+  },
+  creator_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Member",
+    required: true,
+  },
+  status: {
+    type: String,
+    maxlength: 10,
+    required: true,
+    default: "ongoing",
+    enum: ["ongoing", "complete", "end"],
+  },
+  like_by_user_ids: {
+    type: [Schema.Types.ObjectId],
+    ref: "Member",
+  },
+  save_by_user_ids: {
+    type: [Schema.Types.ObjectId],
+    ref: "Member",
+  },
+  currency: {
+    type: String,
+    maxlength: 10,
+    enum: ["USD", "GBP", "EUR", "TWD", "CAD", "AUD"],
+  },
+  budget: {
+    type: Number,
+  },
 });
 
-module.exports = mongoose.model('Event', eventSchema);
+// destination 換成經緯度後要加這行
+// eventSchema.index({ destination: "2dsphere" });
+
+module.exports = mongoose.model("Event", eventSchema);
