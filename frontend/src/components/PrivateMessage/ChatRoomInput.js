469b1fbcf7248884e31097c995eb2904d071705f
黃文新 <r11725018@ntu.edu.tw>
2024-05-04 13:40:35 +0800

    feat: 上傳按鈕（未接api）

--- frontend/src/components/PrivateMessage/ChatRoomInput.js
+++ frontend/src/components/PrivateMessage/ChatRoomInput.js
@@ -1,6 +1,6 @@
 // ChatRoomInput.js
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './ChatRoom.css';
 import { Button, Input, Space } from 'antd';
 import { CameraOutlined ,SendOutlined} from '@ant-design/icons';
@@ -11,11 +11,36 @@ export default function ChatRoomInput({ handleInputChange, inputValue, handleSub
   const handleResize = () => {
     setInputRows(0)
   };
+
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInputChange = (e) => {
+    console.log('Selected file:', e.target.files[0]);
+  };
+
+
   
   return (
       <div className='private-message-input-box'>
         <Space.Compact className='private-message-input'>
-          <Button type="primary" icon={<CameraOutlined />}  style={{ width: '10%' , borderRadius:0}}></Button>
+          <Button
+          type="primary"
+          icon={<CameraOutlined />}
+          style={{ width: '10%', borderRadius: 0 }}
+          onClick={handleButtonClick}
+          >
+          </Button>
+          <input
+            type='file'
+            id='fileInput'
+            style={{ display: 'none' }}
+            ref={fileInputRef}
+            onChange={handleFileInputChange}
+          />
           <Input.TextArea
             autoSize={{ minRows: 1, maxRows: 3 }} // 自動調整大小，最多變為三行
             onChange={handleInputChange}
