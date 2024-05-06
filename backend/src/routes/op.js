834bfc344e20a9171d7505b898464863cf811b05
Samantha Chu <62437040+samanthaChu1123@users.noreply.github.com>
2024-05-06 23:26:21 +0800

    perf: delete redundant code (#80)
    * perf: delete redundant code
    * perf: delete savePic api

--- backend/src/routes/op.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const opController = require("../controllers/op");
-
-router.get("/copylink", opController.copylink);
-
-module.exports = router;
