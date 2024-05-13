fa9512802120ec397d1343e5e52195b5bcde636b
Claire <r12725029@ntu.edu.tw>
2024-05-14 00:28:16 +0800

    feat: add sort, filter api routes

--- backend/src/routes/geolocation.js
+++ backend/src/routes/geolocation.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const geolocationController = require("../controllers/geolocation.js");
 const validateToken = require("../middlewares/validateToken.js");
+const getIpLocation = require("../middlewares/ipLocation.js");
 
-// router.get("/check/:receiver_id", validateToken, chatController.checkChat);
+router.get("/sort", getIpLocation, geolocationController.sortDistance);
+router.get("/filter", getIpLocation, geolocationController.filterDistance);
 
 module.exports = router;
