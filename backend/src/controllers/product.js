c17eade82ed22e4b17efcae31b837132e62b4e42
Claire <r12725029@ntu.edu.tw>
2024-05-22 15:14:20 +0800

    fix: product - .equals to .toString()

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -91,7 +91,7 @@ class productApi {
       delete responseTrans.product_pic;
       responseTrans.product_pic = photoBase64;
 
-      if (user_id !== trans.creator_id.toString()) {
+      if (user_id.toString() !== trans.creator_id.toString()) {
         return res.status(401).json({
           success: false,
           message: "沒有權限編輯該交易",
