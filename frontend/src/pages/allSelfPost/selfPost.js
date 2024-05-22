45a980412d7787454a85520beb4c5be1351e0091
IsFolk <love27929627@gmail.com>
2024-05-22 14:15:21 +0800

    fix: personal function
    因為都已經有放到左下角了，先把post/published下的CreateAllButton先拿掉

--- frontend/src/pages/allSelfPost/selfPost.js
+++ frontend/src/pages/allSelfPost/selfPost.js
@@ -105,7 +105,6 @@ export default function SelfPost() {
                     postTypeToDelete = {selectedPostType}
                 />
             )}
-            <CreateAllButton></CreateAllButton>
         </>
     )
 }
