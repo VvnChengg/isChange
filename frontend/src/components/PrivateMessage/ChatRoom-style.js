c65056e13da68fefab3d18d014a15cbcdf3c648c
r12725023 <jiacian.yang@gmail.com>
2024-05-24 16:41:21 +0800

    feat: chat list style & rwd

---
+++ frontend/src/components/PrivateMessage/ChatRoom-style.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const ChatRoomWrapper = styled.div `
+    width: 100%;
+    max-height: 100%;
+`
+
+export const ChatRoomContainer = styled.div `
+    width: 100%;
+    height: 90px;
+    padding: 10px 0;
+    display: flex;
+
+    border-bottom: ${props => props.showBottom ? `1px solid ${props.theme.main}` : ''};
+
+    cursor: pointer;
+`
+
+export const ChatRoomAvatar = styled.div `
+    height: 100%;
+    aspect-ratio: 1 / 1;
+    border-radius: 50%;
+
+    margin-right: 10px;
+    overflow: hidden;
+`
+
+export const ChatRoomContent = styled.div `
+    width: calc(100% - 80px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const ChatRoomRow = styled.div `
+    display: flex;
+    justify-content: space-between;
+`
--- frontend/src/pages/private-messages/private-message-list.js
+++ frontend/src/pages/private-messages/private-message-list.js
@@ -19,8 +19,6 @@ export default function PrivateMessageList() {
     }, []);
 
     return (
-        <div>
-            <PrivateList />
-        </div>
+        <PrivateList />
     );
 }
