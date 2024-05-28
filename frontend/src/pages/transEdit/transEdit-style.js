12cb50c6760606f23cca8000b87cf0268feda577
IsFolk <love27929627@gmail.com>
2024-05-29 00:51:59 +0800

    fix: trans edit RWD

--- frontend/src/pages/transEdit/transEdit-style.js
+++ frontend/src/pages/transEdit/transEdit-style.js
@@ -12,7 +12,7 @@ export const EditContainer = styled.div `
     display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
+        // justify-content: center;
 
     > * {
         width: 100%;
